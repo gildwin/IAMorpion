460a7f3a86bc93409d54c41b3dacb0f0bd942784
Thomas <tho.voneuw@gmail.com>
2019-05-03 12:32:16 +0200

    Ajout fonction alphaBeta A compléter : le renvoie (qui doit être une action)               Création de maxab et minab

--- src/JoueurIAAlphaBeta.java
+++ src/JoueurIAAlphaBeta.java
@@ -1,11 +1,70 @@
+import java.util.List;
+
 public class JoueurIAAlphaBeta extends JoueurIA{
 
     public JoueurIAAlphaBeta(String name){
         super(name);
     }
 
+    public float utilite(Etat n){
+        return (float)0.2;
+    }
+
+    /*public float max(Etat n, float alpha, float beta){
+        List<Action> actionsPossibles = n.actionsPossibles();
+        int action;
+        for(action = 0; action<actionsPossibles.size(); action++){
+            while(alpha<beta){
+                alpha =
+            }
+        }
+        return alpha;
+    }*/
+
+    public float min(Etat n, float alpha, float beta){
+        return beta;
+    }
+
+    public float alphaBeta(Etat n, float alpha, float beta){
+        List<Action> actionsPossibles = n.actionsPossibles();
+        int joueur = n.getIdJoueurCourant();
+        if(actionsPossibles.isEmpty()){
+            return utilite(n);
+        }
+        else{
+            //Type Max
+            if(this.getID()==joueur){
+                int action;
+                for(action = 0; action<actionsPossibles.size(); action++){
+                    while(alpha<beta){
+                        Etat copieden = n.clone();
+                        copieden.setIdJoueurCourant(copieden.getIdJoueurCourant()+1);
+                        copieden.actionsPossibles().remove(action);
+                        alpha = maxab(alpha, alphaBeta(copieden,alpha,beta));
+                    }
+                }
+                return alpha;
+            }
+            else{
+                if(this.getID()==joueur){
+                    int action;
+                    for(action = 0; action<actionsPossibles.size(); action++){
+                        while(alpha<beta){
+                            Etat copieden = n.clone();
+                            copieden.setIdJoueurCourant(copieden.getIdJoueurCourant()+1);
+                            copieden.actionsPossibles().remove(action);
+                            beta = minab(alpha, alphaBeta(copieden,alpha,beta));
+                        }
+                    }
+                    return beta;
+                }
+            }
+        }
+    }
+
     @Override
-    public Action choisirAction(Etat etat) throws Exception {
+    public Action choisirAction(Etat etat){
+        List<Action> actionsPossibles = etat.actionsPossibles();
         return null;
     }
 
