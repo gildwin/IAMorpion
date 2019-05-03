0154a43e93963ef8893aa73df3dbcc907103e9a9
Thomas <tho.voneuw@gmail.com>
2019-05-03 10:53:03 +0200

    Ajout de la classe JoueurIAAlphaBeta

---
+++ src/JoueurIAAlphaBeta.java
@@ -0,0 +1,12 @@
+public class JoueurIAAlphaBeta extends JoueurIA{
+
+    public JoueurIAAlphaBeta(String name){
+        super(name);
+    }
+
+    @Override
+    public Action choisirAction(Etat etat) throws Exception {
+        return null;
+    }
+
+}
