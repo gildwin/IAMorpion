32766c666a580bd0f9dec93eb1766685cf7e21b3
Thomas <tho.voneuw@gmail.com>
2019-06-02 18:01:16 +0200

    IA parcours itératif terminée 2.0 commentée

--- src/JoueurIAAlphaBeta.java
+++ src/JoueurIAAlphaBeta.java
@@ -2,93 +2,30 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Cette classe représente l'IA qui implémente l'algorithme Alpha-Beta et l'algorithme d'approfondissement itératif
+ */
 public class JoueurIAAlphaBeta extends JoueurIA{
 
+    /**
+     * Constructeur
+     * @param name  Nom choisi pour l'IA
+     */
     public JoueurIAAlphaBeta(String name){
         super(name);
     }
 
-    public Symbole vicEncorePossibleLigne(Plateau plateau, int x){
-        Symbole sym = Symbole.VIDE;
-        int CptSymbConsec = 0;
-        for(int k = 0; k<plateau.getTaille(); k++) {
-            Symbole curSym = plateau.getCase(x,k);
-            if(curSym == sym || curSym == Symbole.VIDE){
-                CptSymbConsec++;
-                if(CptSymbConsec == 4){
-                    return curSym;
-                }
-            }else {
-                CptSymbConsec = 0;
-                sym = curSym;
-            }
-        }
-        return Symbole.VIDE;
-    }
-
-    public Symbole vicEncorePossibleColonne(Plateau plateau, int y){
-        Symbole sym = Symbole.VIDE;
-        int CptSymbConsec = 0;
-        for(int k = 0; k<plateau.getTaille(); k++) {
-            Symbole curSym = plateau.getCase(k,y);
-            if(curSym == sym || curSym == Symbole.VIDE){
-                CptSymbConsec++;
-                if(CptSymbConsec == 4){
-                    return curSym;
-                }
-            }else {
-                CptSymbConsec = 0;
-                sym = curSym;
-            }
-        }
-        return Symbole.VIDE;
-    }
-
-    /*public Symbole vicEncorePossibleDiag(Plateau plateau) {
-        Symbole sym = Symbole.VIDE;
-        int CptSymbConsec = 0;
-        //Diag HGBD
-        for (int k = 0; k < plateau.getTaille(); k++) {
-
-        }
-        return Symbole.VIDE;
-    }*/
-
-    /*public float h(Etat etat){
-        Plateau plateau = etat.getPlateau();
-        //Si quelqu'un gagne, 100 ou -100
-        if (etat.situationCourante() instanceof Victoire) {
-            if(this == ((Victoire) etat.situationCourante()).getVainqueur()){
-                return 100;
-            }
-            else{
-                return -100;
-            }
-        }
-        //Sinon, il faut évaluer la situation
-        else{
-            //Human
-            int nX = 0;
-            //AI
-            int nO = 0;
-            for(int k = 0; k<plateau.getTaille(); k++){
-                Symbole symL = vicEncorePossibleLigne(plateau, k);
-                Symbole symC = vicEncorePossibleColonne(plateau, k);
-                if(symL == Symbole.X){
-                    nX++;
-                }else if(symL == Symbole.O){
-                    nO++;
-                }
-                if(symC == Symbole.X){
-                    nX++;
-                }else if(symC == Symbole.O){
-                    nO++;
-                }
-            }
-            return 100*(nO-2*nX)/(nO+2*nX+1);
-        }
-    }*/
-
+    /**
+     * Fonction qui vérifie s'il y a au moins un certain nombre de pions d'un joueur sur une ligne/colonne/diagonale du plateau
+     * @param nbPions   Nombre de pions minimum sur la ligne/colonne/diagonale
+     * @param symbole   Symbole du pion du joueur
+     * @param x         abscisse du point de départ sur le plateau
+     * @param y         ordonnée du point de départ sur le plateau
+     * @param dX        Déplacement pour la prochaine case à vérifier en abscisse
+     * @param dY        Déplacement pour la prochaine case à vérifier en ordonnée
+     * @param plateau   Plateau sur lequel s'effectue la vérification
+     * @return          vrai s'il y a au moins nbPions sur la ligne/colonne/diagonale et false sinon
+     */
     public boolean auMoins(int nbPions, Symbole symbole, int x, int y, int dX, int dY, Plateau plateau){
         boolean res = false;
         int curX = x;
@@ -108,8 +45,12 @@ public class JoueurIAAlphaBeta extends JoueurIA{
         return res;
     }
 
+    /**
+     * Evalue l'état de la partie à un instant donné
+     * @param etat  Etat de la partie
+     * @return      une évaluation entre -100 et 100 de la partie en cours
+     */
     public int h(Etat etat){
-        //System.out.println("Appel");
         if (etat.situationCourante() instanceof Victoire) {
             if(this == ((Victoire) etat.situationCourante()).getVainqueur()){
                 return 100;
@@ -125,7 +66,6 @@ public class JoueurIAAlphaBeta extends JoueurIA{
             //Gestion lignes et colonnes
             for (int x = 0; x < plateau.getTaille(); x++) {
                 //Gestion lignes
-                //System.out.println("Nouvelle ligne");
                 if (!(auMoins(1, Symbole.O, x, 0, 0, 1, plateau) && auMoins(1, Symbole.X, x, 0, 0, 1, plateau))) {
                     if (auMoins(3, Symbole.X, x, 0, 0, 1, plateau)) {
                         res = res - 20;
@@ -144,7 +84,6 @@ public class JoueurIAAlphaBeta extends JoueurIA{
             }
 
             for (int y = 0; y < plateau.getTaille(); y++) {
-            //System.out.println("Nouvelle colonne");
             //Gestion colonnes
                 if (!(auMoins(1, Symbole.O, 0, y, 1, 0, plateau) && auMoins(1, Symbole.X, 0, y, 1, 0, plateau))) {
                     if (auMoins(3, Symbole.X, 0, y, 1, 0, plateau)) {
@@ -166,7 +105,6 @@ public class JoueurIAAlphaBeta extends JoueurIA{
             int PartPlat3 = plateau.getTaille();
             //Gestion diagonales
             for(int i = 0; (i<plateau.getTaille()) || PartPlat3==3; i++){
-                System.out.println("Nouvelle diagonale montante");
                 //Diagonales montantes
                 if (!(auMoins(1, Symbole.O, ptaille-1, i, -1, 1, plateau) && auMoins(1, Symbole.X, ptaille-1, i, -1, 1, plateau))) {
                     if(auMoins(3, Symbole.O, ptaille-1, i, -1, 1, plateau)){
@@ -189,7 +127,6 @@ public class JoueurIAAlphaBeta extends JoueurIA{
                     }
                 }
                 if(i>0){
-                    System.out.println("Nouvelle diagonale montante");
                     if (!(auMoins(1, Symbole.O, ptaille-1-i, 0, -1, 1, plateau) && auMoins(1, Symbole.X, ptaille-1-i, 0, -1, 1, plateau))) {
                         if(auMoins(3, Symbole.O, ptaille-1-i, 0, -1, 1, plateau)){
                             res = res + 20;
@@ -212,7 +149,6 @@ public class JoueurIAAlphaBeta extends JoueurIA{
                     }
                 }
 
-                System.out.println("Nouvelle diagonale descendante");
                 //Diagonales descendantes
                 if (!(auMoins(1, Symbole.O, 0, i, 1, 1, plateau) && auMoins(1, Symbole.X, 0, i, 1, 1, plateau))) {
                     if(auMoins(3, Symbole.O, 0, i, 1, 1, plateau)){
@@ -235,7 +171,6 @@ public class JoueurIAAlphaBeta extends JoueurIA{
                     }
                 }
                 if(i>0){
-                    System.out.println("Nouvelle diagonale descendante");
                     if (!(auMoins(1, Symbole.O, i, 0, 1, 1, plateau) && auMoins(1, Symbole.X, i,0, 1, 1, plateau))) {
                         if(auMoins(3, Symbole.O, i, 0, 1, 1, plateau)){
                             res = res + 20;
@@ -263,6 +198,13 @@ public class JoueurIAAlphaBeta extends JoueurIA{
         }
     }
 
+    /**
+     * Application de l'algorithme Alpha-Beta vu en cours d'Intelligence Artificielle
+     * @param n         Etat de la partie en cours
+     * @param alpha     Valeur maximum enregistré lors du parcours de l'arbre
+     * @param beta      Valeur minimum enregistré lors du parcours de l'arbre
+     * @return          alpha si le noeud courant est un noeud max et beta sinon
+     */
     public float alphaBeta(Etat n, float alpha, float beta){
         List<Action> actionsPossibles = n.actionsPossibles();
         int joueur = n.getIdJoueurCourant();
@@ -315,6 +257,15 @@ public class JoueurIAAlphaBeta extends JoueurIA{
         }
     }
 
+    /**
+     * Adaptation de l'algorithme Alpha-Beta à une profondeur maximum donné
+     * @param n         Etat de la partie en cours
+     * @param alpha     Valeur maximum enregistré lors du parcours de l'arbre
+     * @param beta      Valeur minimum enregistré lors du parcours de l'arbre
+     * @param curProf   Profondeur actuelle dans le parcours de l'arbre
+     * @param maxProf   Profondeur maximum possiblement atteinte
+     * @return          une évaluation de la partie en cours en fonction du noeud concerné (maximum ou minimum)
+     */
     public float ApprofondissementIteratif(Etat n, float alpha, float beta, int curProf, int maxProf){
         //On est arrivé à une feuille de l'arbre
         if (n.situationCourante() instanceof Victoire) {
@@ -334,12 +285,10 @@ public class JoueurIAAlphaBeta extends JoueurIA{
 
         //On est à une feuille de la longueur courante, mais pas de l'arbre
         if(curProf==maxProf) {
-            //System.out.println("Profondeur courante : " + curProf + " pour une profondeur max  de : " + maxProf + " - Feuille terminal");
            return h(n);
         }
         //En cours de calcul
         else {
-            //System.out.println("Profondeur courante : " + curProf + " pour une profondeur max  de : " + maxProf + " - Noeud");
             //Type Max
             if (this.getID() == joueur) {
                 int action;
@@ -374,6 +323,11 @@ public class JoueurIAAlphaBeta extends JoueurIA{
         }
     }
 
+    /**
+     *
+     * @param etat Etat de la partie
+     * @return     Action choisie par l'algorithme Alpha-Beta ou l'algorithme Approfondissement itératif
+     */
     @Override
     public Action choisirAction(Etat etat){
         List<Action> actionsPossibles = etat.actionsPossibles();
